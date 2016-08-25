5161852aa2e9112362ee42af6bf17e10817a0d43
Nkechi B. Nnaji <nkechi.nnaji@udc.edu>
2016-08-24 23:11:49 -0400

    Change message and port

---
+++ hello-world.js
@@ -0,0 +1,16 @@
+
+var http = require('http');
+
+const PORT = 3000;
+
+function handleRequest(request, response) {
+    response.writeHead(200);
+    response.end('Hello Http from nkechinnaji');
+}
+
+
+var server = http.createServer(handleRequest);
+server.listen(PORT, function(){
+    console.log("Server listening on: http://localhost:%s", PORT);
+});
+
